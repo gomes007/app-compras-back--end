92f80333eea592f4f03c49ab8c97cfb636a53043
Paulo Gomes <gomes7.paulo@gmail.com>
2022-01-08 20:39:43 -0300

    aula 26

--- src/main/java/com/example/cursomc/domain/Cliente.java
+++ src/main/java/com/example/cursomc/domain/Cliente.java
@@ -40,6 +40,8 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	
 	public Cliente() {
@@ -147,6 +149,16 @@ public class Cliente implements Serializable {
 			return false;
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
+	}
+
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 
