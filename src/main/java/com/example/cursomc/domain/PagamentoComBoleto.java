93f5c188004594f105019bbbf6ad1e06b5b483e7
Paulo Gomes <gomes7.paulo@gmail.com>
2022-01-14 17:13:42 -0300

    aula 39

--- src/main/java/com/example/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/example/cursomc/domain/PagamentoComBoleto.java
@@ -18,16 +18,23 @@ public class PagamentoComBoleto extends Pagamento {
 
 	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
+	
+	
 
 	public PagamentoComBoleto() {
 	}
 
+	
+	
 	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
 		super(id, estado, pedido);
 		this.dataPagamento = dataPagamento;
 		this.dataVencimento = dataVencimento;
 	}
 
+	
+	
+	
 	public Date getDataVencimento() {
 		return dataVencimento;
 	}
--- src/main/java/com/example/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/example/cursomc/dto/CategoriaDTO.java
@@ -3,12 +3,19 @@ package com.example.cursomc.dto;
 import java.io.Serializable;
 
 import com.example.cursomc.domain.Categoria;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 
 public class CategoriaDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private Integer id;	
+	private Integer id;
+	
+	@NotEmpty(message = "campo obrigatorio")
+	@Length(min=2, max=80, message = "minimo 2 maximo 80")
 	private String nome;
 	
 	
