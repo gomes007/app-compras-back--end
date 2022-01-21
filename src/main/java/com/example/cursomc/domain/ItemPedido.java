0f4d0084c52aa3c90b72a18f4353f9f97618dec4
Paulo Gomes <gomes7.paulo@gmail.com>
2022-01-21 20:43:09 -0300

    aula 53

--- src/main/java/com/example/cursomc/domain/ItemPedido.java
+++ src/main/java/com/example/cursomc/domain/ItemPedido.java
@@ -52,10 +52,20 @@ public class ItemPedido implements Serializable {
 	public Produto getProduto() {
 		return id.getProduto();
 	}
+	
+	
 
 
 	public ItemPedidoPK getId() {
 		return id;
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 
 
--- src/main/java/com/example/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/example/cursomc/resources/ProdutoResource.java
@@ -2,8 +2,6 @@ package com.example.cursomc.resources;
 
 import java.util.List;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
